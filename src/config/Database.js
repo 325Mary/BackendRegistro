ddb7ffa01864d1d94b6e35af095ff8e331b484b8
325mary <mcordoba577@misena.edu.co>
2024-11-30 10:33:31 -0500

    variables .env

--- src/config/Database.js
+++ src/config/Database.js
@@ -1,11 +1,18 @@
 const mysql =  require('mysql2');
+require('dotenv').config();
 
-const pool= mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'backendRegistro'
-})
+// const pool= mysql.createPool({
+//     host: 'localhost',
+//     user: 'root',
+//     password: 'root',
+//     database: 'backendRegistro'
+// })
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,      
+    user: process.env.DB_USER,       
+    password: process.env.DB_PASSWORD, 
+    database: process.env.DB_NAME      
+});
 
 pool.query('SELECT 1 + 1 AS solution', function(err, row){
     if(err){
